9754f9455adc034b5f7b2c9d3dad6e2b2b11a9ae
Keane1406 <129936029+Keane1406@users.noreply.github.com>
2025-03-13 17:17:01 +1100

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Internet Extender"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "9bnc6929ex"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2875707			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "K480XUERMYPMRLNJ"   // replace XYZ with your channel write API Key
